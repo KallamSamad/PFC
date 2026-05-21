4671d4149a952144a0614723b909552904e417ee
Kallam <kallamsamad@gmail.com>
2026-05-21 20:03:17 +0100

    Added dockerfile2

--- HouseholdUtilitiesWeb/obj/Debug/net9.0/HouseholdUtilitiesWeb.AssemblyInfo.cs
+++ HouseholdUtilitiesWeb/obj/Debug/net9.0/HouseholdUtilitiesWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseholdUtilitiesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a4881a24fb8b5d209aa31632f2703a885b8e02")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseholdUtilitiesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseholdUtilitiesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
